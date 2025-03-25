b183bf7fa550c55ca697e8443e521c24e336fe89
abhishekmankude <abhishekmankude@gmail.com>
2025-03-25 10:23:46 +0530

    getters and setters

---
+++ Getter_Setters/src/com/xworkz/GettersSetters/example.java
@@ -0,0 +1,18 @@
+package com.xworkz.GettersSetters;
+
+public class example {
+
+    // Private variable
+    private String name;
+
+    // Getter method
+    public  String getName() {
+        return name;
+    }
+
+    // Setter method
+    public void setName(String newName) {
+        this.name = newName;
+    }
+}
+
---
+++ Getter_Setters/src/com/xworkz/GettersSetters/main.java
@@ -0,0 +1,14 @@
+package com.xworkz.GettersSetters;
+
+public class main {
+    public static void main(String[] args) {
+        // Creating an object of Person
+        example person = new example();
+
+        // Using setter to assign value
+        person.setName("Abhishek");
+
+        // Using getter to retrieve value
+        System.out.println("Name: " + person.getName());
+    }
+}
