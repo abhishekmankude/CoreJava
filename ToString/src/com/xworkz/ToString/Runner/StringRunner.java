2d09d9ee26668e0f611851512a0e62071f49bb4a
abhishekmankude <abhishekmankude@gmail.com>
2025-04-10 00:53:50 +0530

    To String

--- ToString/src/com/xworkz/ToString/Runner/StringRunner.java
+++ ToString/src/com/xworkz/ToString/Runner/StringRunner.java
@@ -17,411 +17,407 @@ public class StringRunner {
         System.out.println(malleshwaram);
         System.out.println();
 
-        Fan fan = new Fan("Ceiling", 1500, 100);
+        Fan fan = new Fan("Tower", 1200, 80);
         System.out.println(fan);
         System.out.println();
 
-        Light light = new Light("Philips", "Tubelight", 250);
+        Light light = new Light("Havells", "LED", 300);
         System.out.println(light);
         System.out.println();
 
-        Shirt shirt = new Shirt("Allen Solly", "pink", 1299);
+        Shirt shirt = new Shirt("Peter England", "blue", 1599);
         System.out.println(shirt);
         System.out.println();
 
-        Pant pant = new Pant("MudWear", "Grey", 1799);
+        Pant pant = new Pant("Levi's", "black", 2499);
         System.out.println(pant);
         System.out.println();
 
-        Bag bag = new Bag("SkyBags", "Black", 25);
+        Bag bag = new Bag("American Tourister", "Navy Blue", 30);
         System.out.println(bag);
         System.out.println();
 
-        Book book = new Book("Classmate", 100, 100);
+        Book book = new Book("Navneet", 200, 120);
         System.out.println(book);
         System.out.println();
 
-        IceCream icecream = new IceCream("Amul", "Vanilla", 150);
+        IceCream icecream = new IceCream("Kwality Walls", "Chocolate", 100);
         System.out.println(icecream);
         System.out.println();
 
-        Zoo zoo = new Zoo("Pilikula", "Rohan", 50);
+        Zoo zoo = new Zoo("Bannerghatta", "Rahul", 75);
         System.out.println(zoo);
         System.out.println();
 
-        TV tv = new TV("Sharp", 24, 15000);
+        TV tv = new TV("Sony", 32, 22000);
         System.out.println(tv);
         System.out.println();
 
-        Cake cake = new Cake("JustBake", "ButtreScotch", 500);
+        Cake cake = new Cake("Theobroma", "Red Velvet", 800);
         System.out.println(cake);
         System.out.println();
 
-        Samosa samosa = new Samosa("Chat", "Traingle", 25);
+        Samosa samosa = new Samosa("Haldiram's", "Pyramid", 20);
         System.out.println(samosa);
         System.out.println();
 
-        Language language = new Language("Tulu", "Mangalore", 1000);
+        Language language = new Language("Kannada", "Bangalore", 5000);
         System.out.println(language);
         System.out.println();
 
-        BMTC bmtc = new BMTC("Bangalore", "Attiguppe", 12);
+        BMTC bmtc = new BMTC("Bengaluru", "Majestic", 25);
         System.out.println(bmtc);
         System.out.println();
 
-        WoodenBox woodenBox = new WoodenBox("wood", "square", 1);
+        WoodenBox woodenBox = new WoodenBox("Teak", "rectangular", 2);
         System.out.println(woodenBox);
         System.out.println();
 
-        Bottle bottle = new Bottle("Milton", "White", 2);
+        Bottle bottle = new Bottle("Borosil", "Transparent", 1);
         System.out.println(bottle);
         System.out.println();
 
-        Park park = new Park("HBCS Park", "ChandraLayout", 1750);
+        Park park = new Park("Cubbon Park", "Central Bangalore", 3000);
         System.out.println(park);
         System.out.println();
 
-        Laptop laptop = new Laptop("Asus", "i7", 55000);
+        Laptop laptop = new Laptop("Dell", "i5", 45000);
         System.out.println(laptop);
         System.out.println();
 
-        Mobile mobile = new Mobile("Oppo", "A37", 19900);
+        Mobile mobile = new Mobile("Samsung", "Galaxy M32", 15999);
         System.out.println(mobile);
         System.out.println();
 
-        KeyBoard keyBoard = new KeyBoard("Zebronics", 108, 599);
+        KeyBoard keyBoard = new KeyBoard("Logitech", 104, 899);
         System.out.println(keyBoard);
         System.out.println();
 
-        Shoe shoe = new Shoe("WoodLand", 11, 2199);
+        Shoe shoe = new Shoe("Nike", 10, 3499);
         System.out.println(shoe);
         System.out.println();
 
-        Console console = new Console("PS5", "Microsoft", 25000);
+        Console console = new Console("Xbox Series X", "Microsoft", 49999);
         System.out.println(console);
         System.out.println();
 
-        Bed bed = new Bed("Foldable", "sponge", 699);
+        Bed bed = new Bed("King Size", "Memory Foam", 12999);
         System.out.println(bed);
         System.out.println();
 
-        Metro metro = new Metro("bangalore", "purple", 10);
+        Metro metro = new Metro("Delhi", "Yellow", 15);
         System.out.println(metro);
         System.out.println();
 
-        Money money = new Money("INR", "India", 100);
+        Money money = new Money("USD", "United States", 50);
         System.out.println(money);
         System.out.println();
 
-        Bank bank = new Bank("Karnatak Bank", "Bangalore", 150000);
+        Bank bank = new Bank("SBI", "Mumbai", 500000);
         System.out.println(bank);
         System.out.println();
 
-        Money atm = new Money("BOB", "Mangalore", 10001);
+        Money atm = new Money("ICICI", "Delhi", 20000);
         System.out.println(atm);
         System.out.println();
 
-        PetrolPump petrolPump = new PetrolPump("Pumpwell", "highPower", 101);
+        PetrolPump petrolPump = new PetrolPump("HP", "Highway", 102);
         System.out.println(petrolPump);
         System.out.println();
 
-        SunGlasses sunGlasses = new SunGlasses("Vision", "black", 1299);
+        SunGlasses sunGlasses = new SunGlasses("Ray-Ban", "Aviator", 5999);
         System.out.println(sunGlasses);
         System.out.println();
 
-        Scooter scooter = new Scooter("Honda", "Activa 6g", 120000);
+        Scooter scooter = new Scooter("TVS", "Jupiter", 85000);
         System.out.println(scooter);
         System.out.println();
 
-        Lamp lamp = new Lamp("Hanging", "yellow", 150);
+        Lamp lamp = new Lamp("Table", "warm white", 499);
         System.out.println(lamp);
         System.out.println();
 
-        Stand stand = new Stand("Wooden", "brown", 1599);
+        Stand stand = new Stand("Metal", "black", 999);
         System.out.println(stand);
         System.out.println();
 
-        Remote remote = new Remote("Sony", "black", 250);
+        Remote remote = new Remote("LG", "silver", 350);
         System.out.println(remote);
         System.out.println();
 
-        Charger battery = new Charger("Lithium-ion", 2, 9);
+        Charger battery = new Charger("Li-Polymer", 3, 10);
         System.out.println(battery);
         System.out.println();
 
-        Tshirt tshirt = new Tshirt("USPOLO", "red", 1100);
+        Tshirt tshirt = new Tshirt("Adidas", "black", 1999);
         System.out.println(tshirt);
         System.out.println();
 
-        Chess chess = new Chess("Wooden", "white", 200);
+        Chess chess = new Chess("Staunton", "ebony", 2500);
         System.out.println(chess);
         System.out.println();
 
-        Pen pen = new Pen("Cello", "blue", 10);
+        Pen pen = new Pen("Parker", "black", 150);
         System.out.println(pen);
         System.out.println();
 
-        Pencil pencil = new Pencil("Natraj", "Penpencil", 15);
+        Pencil pencil = new Pencil("Apsara", "HB", 10);
         System.out.println(pencil);
         System.out.println();
 
-        Chappal chappal = new Chappal("Slippers", "M", 150);
+        Chappal chappal = new Chappal("Paragon", "L", 499);
         System.out.println(chappal);
         System.out.println();
 
-        Driver driver = new Driver("Vijay", "89990", 5);
+        Driver driver = new Driver("Ramesh", "9876543210", 10);
         System.out.println(driver);
         System.out.println();
 
-        Table table = new Table("Round", "Plastic", 15000);
+        Table table = new Table("Dining", "Wooden", 12000);
         System.out.println(table);
         System.out.println();
 
-        Watch watch = new Watch("Titan", "casual", 9000);
+        Watch watch = new Watch("Casio", "Digital", 1999);
         System.out.println(watch);
         System.out.println();
 
-        Oven oven = new Oven("Electric", 50, 15000);
+        Oven oven = new Oven("Microwave", 25, 8999);
         System.out.println(oven);
         System.out.println();
 
-        Speaker speaker = new Speaker("JBL", "Soundbar", 399);
+        Speaker speaker = new Speaker("Bose", "Bluetooth", 5999);
         System.out.println(speaker);
         System.out.println();
 
-        HeadPhone headPhone = new HeadPhone("Boat", "Earbuds", 1299);
+        HeadPhone headPhone = new HeadPhone("Sony", "Over-ear", 2999);
         System.out.println(headPhone);
         System.out.println();
 
-        NoteBook noteBook = new NoteBook("Doms", 200, "Long");
+        NoteBook noteBook = new NoteBook("Classmate", 120, "A4");
         System.out.println(noteBook);
         System.out.println();
 
-        Socks socks = new Socks("Jockey", "black", 80);
+        Socks socks = new Socks("Puma", "white", 199);
         System.out.println(socks);
         System.out.println();
 
-        Wallet wallet = new Wallet("Titan", "Leather", 5000);
+        Wallet wallet = new Wallet("Wildcraft", "Nylon", 999);
         System.out.println(wallet);
         System.out.println();
 
-        ToothPaste toothPaste = new ToothPaste("Colgate", "menthol", 50);
+        ToothPaste toothPaste = new ToothPaste("Pepsodent", "Mint", 75);
         System.out.println(toothPaste);
         System.out.println();
 
-        Soap soap = new Soap("Santoor", "BarSaop", 35);
+        Soap soap = new Soap("Dove", "Beauty Bar", 45);
         System.out.println(soap);
         System.out.println();
 
-        Drone drone = new Drone("Samsung", "HD", 19000);
+        Drone drone = new Drone("DJI", "Mavic", 89999);
         System.out.println(drone);
         System.out.println();
 
-        Belt belt = new Belt("Leather", "Large", 500);
+        Belt belt = new Belt("Allen Solly", "Medium", 799);
         System.out.println(belt);
         System.out.println();
 
-        Torch torch = new Torch("Nippon", 300, 250);
+        Torch torch = new Torch("Eveready", 500, 199);
         System.out.println(torch);
         System.out.println();
 
-        PowerBank powerBank = new PowerBank("Vivo", 20000, 1799);
+        PowerBank powerBank = new PowerBank("Mi", 10000, 999);
         System.out.println(powerBank);
         System.out.println();
 
-        IronBox ironBox = new IronBox("Philips", 20, 990);
+        IronBox ironBox = new IronBox("Bajaj", 1000, 1299);
         System.out.println(ironBox);
         System.out.println();
 
-        Clock clock = new Clock("Sonata", "Fashion", 660);
+        Clock clock = new Clock("Titan", "Wall", 1299);
         System.out.println(clock);
         System.out.println();
 
-        Ticket ticket = new Ticket("MCStan Concert", "25/4", 2000);
+        Ticket ticket = new Ticket("Coldplay Concert", "15/12", 5000);
         System.out.println(ticket);
         System.out.println();
 
-        Coin coin = new Coin("Pice", "Gold", 1902);
+        Coin coin = new Coin("Rupee", "Silver", 1947);
         System.out.println(coin);
         System.out.println();
 
-        Pharmacy pharmacy = new Pharmacy("Mediplus", "Nayandalli", 4);
+        Pharmacy pharmacy = new Pharmacy("Apollo", "MG Road", 5);
         System.out.println(pharmacy);
         System.out.println();
 
-        Mouse mouse = new Mouse("Artic Fox", "Wired", 249);
+        Mouse mouse = new Mouse("Logitech", "Wireless", 599);
         System.out.println(mouse);
         System.out.println();
 
-        Fridge fridge = new Fridge("LG", 30, 50000);
+        Fridge fridge = new Fridge("Whirlpool", 250, 35000);
         System.out.println(fridge);
         System.out.println();
 
-        Towel towel = new Towel("Cotton", "White", 150);
+        Towel towel = new Towel("Microfiber", "Beige", 299);
         System.out.println(towel);
         System.out.println();
 
-        USB usb = new USB("SanDisk", 64, "USB 3.0");
+        USB usb = new USB("Samsung", 128, "USB 3.1");
         System.out.println(usb);
         System.out.println();
 
-        Wire wire = new Wire("Copper", 10, "Red");
+        Wire wire = new Wire("Aluminum", 15, "Black");
         System.out.println(wire);
         System.out.println();
 
-        Shampoo shampoo = new Shampoo("Head & Shoulders", 180, "Mint");
+        Shampoo shampoo = new Shampoo("Pantene", 200, "Smooth");
         System.out.println(shampoo);
         System.out.println();
 
-        Helmet helmet = new Helmet("Vega", "M", "Red");
+        Helmet helmet = new Helmet("Studds", "L", "Black");
         System.out.println(helmet);
         System.out.println();
 
-        Printer printer = new Printer("HP", true, 20);
+        Printer printer = new Printer("Canon", false, 30);
         System.out.println(printer);
         System.out.println();
 
-        Camera camera = new Camera("Canon", 24, true);
+        Camera camera = new Camera("Nikon", 32, false);
         System.out.println(camera);
         System.out.println();
 
-        Mat mat = new Mat("Rubber", "Rectangle", 5);
+        Mat mat = new Mat("Yoga", "Blue", 6);
         System.out.println(mat);
         System.out.println();
 
-        Dustbin bin = new Dustbin("Plastic", 15, "Green");
+        Dustbin bin = new Dustbin("Metal", 20, "Silver");
         System.out.println(bin);
         System.out.println();
 
-        Mixer mixer = new Mixer("Preethi", 3, 3200);
+        Mixer mixer = new Mixer("Bajaj", 5, 2499);
         System.out.println(mixer);
         System.out.println();
 
-        LunchBox box = new LunchBox("Cello", 3, 600);
+        LunchBox box = new LunchBox("Milton", 2, 450);
         System.out.println(box);
         System.out.println();
 
-        Calender calender = new Calender("2025", "Nature", 150);
+        Calender calender = new Calender("2024", "Wildlife", 99);
         System.out.println(calender);
         System.out.println();
 
-        Mug mug = new Mug("Ceramic", "Blue", 250);
+        Mug mug = new Mug("Porcelain", "White", 199);
         System.out.println(mug);
         System.out.println();
 
-        Mirror mirror = new Mirror("Oval", 60, 900);
+        Mirror mirror = new Mirror("Round", 45, 799);
         System.out.println(mirror);
         System.out.println();
 
-        Plate plate = new Plate("Steel", 28, 120);
+        Plate plate = new Plate("Ceramic", 30, 199);
         System.out.println(plate);
         System.out.println();
 
-        Stapler stapler = new Stapler("Kangaroo", 100, 90);
+        Stapler stapler = new Stapler("Butterfly", 50, 120);
         System.out.println(stapler);
         System.out.println();
 
-        Cap cap = new Cap("Black", "Puma", 500);
+        Cap cap = new Cap("Navy", "Adidas", 799);
         System.out.println(cap);
         System.out.println();
 
-        Umbrella umbrella = new Umbrella("Blue", 60, 350);
+        Umbrella umbrella = new Umbrella("Black", 55, 499);
         System.out.println(umbrella);
         System.out.println();
 
-        Charger ch = new Charger("Samsung", 25, 1200);
+        Charger ch = new Charger("OnePlus", 30, 1500);
         System.out.println(ch);
         System.out.println();
 
-        Marker marker = new Marker("Blue", "Faber-Castell", 30);
+        Marker marker = new Marker("Black", "Camlin", 25);
         System.out.println(marker);
         System.out.println();
 
-        Sofa sofa = new Sofa("Leather", 3, 9500);
+        Sofa sofa = new Sofa("Fabric", 5, 15000);
         System.out.println(sofa);
         System.out.println();
 
-        Tripod tripod = new Tripod("Aluminum", 150, 1200);
+        Tripod tripod = new Tripod("Carbon Fiber", 180, 2500);
         System.out.println(tripod);
         System.out.println();
 
-        Guitar guitar = new Guitar("Rock", "Yamaha", 4500);
+        Guitar guitar = new Guitar("Acoustic", "Fender", 12000);
         System.out.println(guitar);
         System.out.println();
 
-        ToyCar toyCar = new ToyCar("Blue", "HotWheels", 150);
+        ToyCar toyCar = new ToyCar("Red", "Matchbox", 99);
         System.out.println(toyCar);
         System.out.println();
 
-        Ruler ruler = new Ruler(30, "Plastic", 20);
+        Ruler ruler = new Ruler(15, "Metal", 50);
         System.out.println(ruler);
         System.out.println();
 
-        Candle candle = new Candle("Lavender", 150, 299);
+        Candle candle = new Candle("Vanilla", 100, 199);
         System.out.println(candle);
         System.out.println();
 
-        Door door = new Door("Wood", 210, 5500);
+        Door door = new Door("Fiber", 200, 4500);
         System.out.println(door);
         System.out.println();
 
-        DiningTable diningTable= new DiningTable("Round", 4, 8900);
+        DiningTable diningTable= new DiningTable("Rectangle", 6, 15000);
         System.out.println(diningTable);
         System.out.println();
 
-        Facewash facewash = new Facewash("Himalaya", 100, 150);
+        Facewash facewash = new Facewash("Garnier", 150, 120);
         System.out.println(facewash);
         System.out.println();
 
-        Ipl ipl = new Ipl("Chennai Super Kings", "MS Dhoni", 5);
+        Ipl ipl = new Ipl("Mumbai Indians", "Rohit Sharma", 5);
         System.out.println(ipl);
         System.out.println();
 
-        CreditCard creditCard = new CreditCard("HDFC", 12345678, 150000);
+        CreditCard creditCard = new CreditCard("ICICI", 87654321, 100000);
         System.out.println(creditCard);
         System.out.println();
 
-        Rice rice = new Rice("Basmati", 5, 450);
+        Rice rice = new Rice("Sona Masoori", 10, 600);
         System.out.println(rice);
         System.out.println();
 
-        Chapati chapati = new Chapati("Wheat", 10, 50);
+        Chapati chapati = new Chapati("Multigrain", 15, 75);
         System.out.println(chapati);
         System.out.println();
 
-        Milk milk = new Milk("Nandini", 500, 25);
+        Milk milk = new Milk("Amul", 1, 50);
         System.out.println(milk);
         System.out.println();
 
-        Avocado
-
-                apple = new Avocado
-
-                ("Fuji", 200, 35);
+        Avocado apple = new Avocado("Hass", 150, 100);
         System.out.println(apple);
         System.out.println();
 
-        Browser browser = new Browser("Chrome", "123.4", 2024);
+        Browser browser = new Browser("Firefox", "115.0", 2023);
         System.out.println(browser);
         System.out.println();
 
-        Port port = new Port("USB", 3, 400);
+        Port port = new Port("HDMI", 2, 300);
         System.out.println(port);
         System.out.println();
 
-        Charger adapter = new Charger("Lenovo", 65, 1200);
+        Charger adapter = new Charger("Apple", 20, 1500);
         System.out.println(adapter);
         System.out.println();
 
-        Jet jet = new Jet("F-22 Raptor", 2410, 1);
+        Jet jet = new Jet("Boeing 747", 900, 2);
         System.out.println(jet);
         System.out.println();
 
-        Star star = new Star("Sirius", 9940, 8);
+        Star star = new Star("Polaris", 6000, 5);
         System.out.println(star);
         System.out.println();
 
-        Ocean ocean = new Ocean("Pacific", 10911, 168723000);
+        Ocean ocean = new Ocean("Atlantic", 8486, 106460000);
         System.out.println(ocean);
         System.out.println();
 
